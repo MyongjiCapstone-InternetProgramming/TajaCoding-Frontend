4fe9b9b51a8faad22acfe6073b9a8421a1c121ca
Jinjeongjun <wlswjdwns1@naver.com>
2024-05-13 22:51:29 +0900

    0513 jun

--- src/App.js
+++ src/App.js
@@ -9,21 +9,25 @@ import Origin from './components/Origin';
 import Custom from './components/Custom';
 import WordQuiz from './components/WordQuiz';
 import WrongAnswer from './components/WrongAnswer';
-import PulicConceptBlock from './components/PulicConceptBlock'
+import WrongAnswerRetry from './components/WrongAndwerRetry';
+import PulicConceptBlock from './components/PulicConceptBlock';
+import WordQuizStart from './components/WordQuizStart';
 
 const App = () => {
   return (
     <div className="app">
-      <StatusBar/>
+      <StatusBar />
       <Routes>
-        <Route path='/' element={<Main/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/signup' element={<Signup/>}/>
-        <Route path='/origin' element={<Origin/>}/>
-        <Route path='/custom' element={<Custom/>}/>
-        <Route path='/wordquiz' element={<WordQuiz/>}/>
-        <Route path='/wronganswer' element={<WrongAnswer/>}/>
-        <Route path='/pulicconceptblock' element={<PulicConceptBlock/>}/>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/origin" element={<Origin />} />
+        <Route path="/custom" element={<Custom />} />
+        <Route path="/wordquiz" element={<WordQuiz />} />
+        <Route path="/wordquizstart" element={<WordQuizStart />} />
+        <Route path="/wronganswer" element={<WrongAnswer />} />
+        <Route path="/wronganswerretry" element={<WrongAnswerRetry />} />
+        <Route path="/pulicconceptblock" element={<PulicConceptBlock />} />
       </Routes>
     </div>
   );
--- src/components/WordQuiz.js
+++ src/components/WordQuiz.js
@@ -1,43 +1,139 @@
 // 담당자 : 은희
-//수업시간 내용 : onmouseover 사용 
+//수업시간 내용 : onmouseover 사용
 
-import { Link } from "react-router-dom";
-import UserNav from "./UserNav";
+import { Link } from 'react-router-dom';
+import UserNav from './UserNav';
 
-export default function WordQuiz(){
-    return(
-        <div style={{height: '100%'}}>
-            <UserNav/>
-            <div style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'flex-start',
-                alignItems: 'flex-start',
-                height: '40%'
-            }}>
-                <div style={{marginTop: '30px', padding: '0.5rem 8rem', fontSize: '20px', color: 'white'}}
-                     onMouseOver={(e) => e.target.style.color = 'limegreen'}
-                     onMouseOut={(e) => e.target.style.color = 'white'}>SELECT * FROM 긴글 코딩 연습;
-                </div>
-                <div><Link to="/pulicconceptBlock" style={{padding: '0.5rem 8rem', fontSize: '20px', color: 'white', textDecoration: 'none'}} onMouseOver={(e) => (e.target.style.color = 'limegreen')} onMouseOut={(e) => (e.target.style.color = 'white')}>SELECT * FROM 빈칸 퀴즈;</Link></div>
-                <div style={{padding: '0.5rem 8rem', fontSize: '20px', color: 'white'}} onMouseOver={(e) => e.target.style.color = 'limegreen'} onMouseOut={(e) => e.target.style.color = 'white'}>SELECT * FROM 개념 퀴즈;
-                </div>
-            </div>
-
-            <div style={{display: 'flex', padding: '5rem', position: 'absolute', top: '120px', right: '40px'}}>
-                <div style={{marginRight: '3rem', fontSize: '1.8rem', color: 'white'}}
-                     onMouseOver={(e) => e.target.style.color = 'limegreen'}
-                     onMouseOut={(e) => e.target.style.color = 'white'}>COMPUTER_SCIENCE</div>
-                <div style={{marginRight: '3rem', fontSize: '1.8rem', color: 'white'}} onMouseOver={(e) => e.target.style.color = 'limegreen'} onMouseOut={(e) => e.target.style.color = 'white'}>JAVA</div>
-                <div style={{marginRight: '3rem', fontSize: '1.8rem', color: 'white'}} onMouseOver={(e) => e.target.style.color = 'limegreen'} onMouseOut={(e) => e.target.style.color = 'white'}>PYTHON</div>
-                <div style={{marginRight: '3rem', fontSize: '1.8rem', color: 'white'}} onMouseOver={(e) => e.target.style.color = 'limegreen'} onMouseOut={(e) => e.target.style.color = 'white'}>C</div>
-                <div style={{fontSize: '1.8rem', color: 'white'}} onMouseOver={(e) => e.target.style.color = 'limegreen'} onMouseOut={(e) => e.target.style.color = 'white'}>C++</div>
-            </div>
-
-            <div style={{display:'flex', justifyContent:'space-evenly', marginTop:'0rem', marginBottom:'6rem'}}>
-                <div style={{border:'0.5rem dashed white', padding:'8rem 10rem', fontSize:'2rem'}}>시작하기</div>
-                <Link style={{border:"0.5rem dashed white", padding:'8rem 10rem', fontSize:'2rem'}} to={'/wronganswer'} className="link-tag">내 오답노트</Link>            </div>
+export default function WordQuiz() {
+  return (
+    <div style={{ height: '100%' }}>
+      <UserNav />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'flex-start',
+          alignItems: 'flex-start',
+          height: '40%',
+        }}
+      >
+        <div
+          style={{
+            marginTop: '30px',
+            padding: '0.5rem 8rem',
+            fontSize: '20px',
+            color: 'white',
+          }}
+          onMouseOver={(e) => (e.target.style.color = 'limegreen')}
+          onMouseOut={(e) => (e.target.style.color = 'white')}
+        >
+          SELECT * FROM 긴글 코딩 연습;
         </div>
-    )
-}
+        <div>
+          <Link
+            to="/pulicconceptBlock"
+            style={{
+              padding: '0.5rem 8rem',
+              fontSize: '20px',
+              color: 'white',
+              textDecoration: 'none',
+            }}
+            onMouseOver={(e) => (e.target.style.color = 'limegreen')}
+            onMouseOut={(e) => (e.target.style.color = 'white')}
+          >
+            SELECT * FROM 빈칸 퀴즈;
+          </Link>
+        </div>
+        <div
+          style={{
+            padding: '0.5rem 8rem',
+            fontSize: '20px',
+            color: 'limegreen',
+          }}
+          onMouseOut={(e) => (e.target.style.color = 'limegreen')}
+          onMouseOver={(e) => (e.target.style.color = 'white')}
+        >
+          SELECT * FROM 개념 퀴즈;
+        </div>
+      </div>
 
+      <div
+        style={{
+          display: 'flex',
+          padding: '5rem',
+          position: 'absolute',
+          top: '120px',
+          right: '40px',
+        }}
+      >
+        <div
+          style={{ marginRight: '3rem', fontSize: '1.8rem', color: 'white' }}
+          onMouseOver={(e) => (e.target.style.color = 'limegreen')}
+          onMouseOut={(e) => (e.target.style.color = 'white')}
+        >
+          COMPUTER_SCIENCE
+        </div>
+        <div
+          style={{ marginRight: '3rem', fontSize: '1.8rem', color: 'white' }}
+          onMouseOver={(e) => (e.target.style.color = 'limegreen')}
+          onMouseOut={(e) => (e.target.style.color = 'white')}
+        >
+          JAVA
+        </div>
+        <div
+          style={{ marginRight: '3rem', fontSize: '1.8rem', color: 'white' }}
+          onMouseOver={(e) => (e.target.style.color = 'limegreen')}
+          onMouseOut={(e) => (e.target.style.color = 'white')}
+        >
+          PYTHON
+        </div>
+        <div
+          style={{ marginRight: '3rem', fontSize: '1.8rem', color: 'white' }}
+          onMouseOver={(e) => (e.target.style.color = 'limegreen')}
+          onMouseOut={(e) => (e.target.style.color = 'white')}
+        >
+          C
+        </div>
+        <div
+          style={{ fontSize: '1.8rem', color: 'white' }}
+          onMouseOver={(e) => (e.target.style.color = 'limegreen')}
+          onMouseOut={(e) => (e.target.style.color = 'white')}
+        >
+          C++
+        </div>
+      </div>
+
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          marginTop: '0rem',
+          marginBottom: '6rem',
+        }}
+      >
+        <Link
+          style={{
+            border: '0.5rem dashed white',
+            padding: '8rem 10rem',
+            fontSize: '2rem',
+          }}
+          to={'/wordquizstart'}
+          className="link-tag"
+        >
+          시작하기
+        </Link>
+        <Link
+          style={{
+            border: '0.5rem dashed white',
+            padding: '8rem 10rem',
+            fontSize: '2rem',
+          }}
+          to={'/wronganswer'}
+          className="link-tag"
+        >
+          내 오답노트
+        </Link>
+      </div>
+    </div>
+  );
+}
