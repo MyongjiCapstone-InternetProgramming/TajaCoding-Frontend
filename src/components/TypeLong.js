b86e7f8761f0664d53085a54ca237a3f0a3e25d4
ChaeDoll <dlacodbs0627@gmail.com>
2024-05-17 02:05:22 +0900

    240517 chae timer alyout

--- src/components/TypeLong.js
+++ src/components/TypeLong.js
@@ -1,26 +1,19 @@
+import { useState } from "react";
 import UserNav from "./UserNav";
+import TypingLayout from "./layout/typinglayout";
 
 export default function TypeLong(){
+    const [timer, setTimer] = useState(90);
     return (
         <div style={{height: '100%'}}>
             <UserNav/>
-            <div style={{display:'flex', height:'100%', flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
-                <div style={{fontSize:'1.5rem' ,display:'flex', flexDirection:'column', justifyContent:'flex-end', alignItems:'center', flex:1, height:'75%', borderRight:'0.1rem solid #FFFFFF99'}}>
-                    <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
-                        <div style={{borderBottom:'0.1rem solid #FFFFFF99', width:'6.2rem', textAlign:'center', paddingBottom:'0.3rem', marginBottom:'0.3rem'}}>진행시간</div>
-                        <div>02 : 34</div>
-                    </div>
-                    <div style={{display:'flex', flexDirection:'column', alignItems:'center', margin:'3rem 0'}}>
-                        <div style={{borderBottom:'0.1rem solid #FFFFFF99', width:'6.2rem', textAlign:'center', paddingBottom:'0.3rem', marginBottom:'0.3rem'}}>정확도</div>
-                        <div>92%</div>
-                    </div>
-                </div>
+            <TypingLayout timer={timer}>
                 <div style={{display:'flex', flex:6, height:'100%', justifyContent:'center'}}>
                     <div style={{backgroundColor:'red', width:'85%'}}>
                         <div></div>
                     </div>
                 </div>
-            </div>
+            </TypingLayout>
         </div>
     )
 }
---
+++ src/components/layout/typinglayout.js
@@ -0,0 +1,21 @@
+const TypingLayout = (props) => {
+    const timer = props.timer;
+    const minute = parseInt(timer/60);
+    const second = timer%60;
+    return (
+        <div style={{display:'flex', height:'100%', flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
+        <div style={{fontSize:'1.5rem' ,display:'flex', flexDirection:'column', justifyContent:'flex-end', alignItems:'center', flex:1, height:'75%', borderRight:'0.1rem solid #FFFFFF99'}}>
+            <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
+                <div style={{borderBottom:'0.1rem solid #FFFFFF99', width:'6.2rem', textAlign:'center', paddingBottom:'0.3rem', marginBottom:'0.3rem'}}>진행시간</div>
+                <div>{("0"+minute).slice(-2)+" : "+("0"+second).slice(-2)}</div>
+            </div>
+            <div style={{display:'flex', flexDirection:'column', alignItems:'center', margin:'3rem 0'}}>
+                <div style={{borderBottom:'0.1rem solid #FFFFFF99', width:'6.2rem', textAlign:'center', paddingBottom:'0.3rem', marginBottom:'0.3rem'}}>정확도</div>
+                <div>92%</div>
+            </div>
+        </div>
+        {props.children}
+    </div>
+    )
+}
+export default TypingLayout
