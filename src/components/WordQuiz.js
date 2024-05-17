6dc838eaacd1f3ab47986d6dfd3a0c54fe2b36a4
Jinjeongjun <wlswjdwns1@naver.com>
2024-05-18 02:28:41 +0900

    0518 0228 jun

--- src/components/WordQuiz.js
+++ src/components/WordQuiz.js
@@ -8,133 +8,27 @@ export default function WordQuiz() {
   return (
     <div style={{ height: '100%' }}>
       <UserNav />
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'flex-start',
-          alignItems: 'flex-start',
-          height: '40%',
-        }}
-      >
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', height: '40%'}}>
         <Link to={'/longcode'} style={{textDecoration:'none'}}>
-        <div
-          style={{
-            marginTop: '30px',
-            padding: '0.5rem 8rem',
-            fontSize: '20px',
-            color: 'white',
-          }}
-          onMouseOver={(e) => (e.target.style.color = 'lime')}
-          onMouseOut={(e) => (e.target.style.color = 'white')}
-        >
-          SELECT * FROM 긴글 코딩 연습;
-        </div>
+          <div style={{ marginTop: '30px', padding: '0.5rem 8rem', fontSize: '20px', color: 'white'}} onMouseOver={(e) => (e.target.style.color = 'lime')} onMouseOut={(e) => (e.target.style.color = 'white')}>SELECT * FROM 긴글 코딩 연습;</div>
         </Link>
         <div>
-          <Link
-            to="/blockcode"
-            style={{
-              padding: '0.5rem 8rem',
-              fontSize: '20px',
-              color: 'white',
-              textDecoration: 'none',
-            }}
-            onMouseOver={(e) => (e.target.style.color = 'lime')}
-            onMouseOut={(e) => (e.target.style.color = 'white')}
-          >
-            SELECT * FROM 빈칸 퀴즈;
-          </Link>
+          <Link to="/blockcode" style={{ padding: '0.5rem 8rem', fontSize: '20px', color: 'white', textDecoration: 'none', }} onMouseOver={(e) => (e.target.style.color = 'lime')} onMouseOut={(e) => (e.target.style.color = 'white')}>SELECT * FROM 빈칸 퀴즈;</Link>
         </div>
-        <div
-          style={{
-            padding: '0.5rem 8rem',
-            fontSize: '20px',
-            color: 'limegreen',
-          }}
-          // onMouseOut={(e) => (e.target.style.color = 'limegreen')}
-          // onMouseOver={(e) => (e.target.style.color = 'white')}
-        >
-          SELECT * FROM 개념 퀴즈;
-        </div>
+        <div style={{ padding: '0.5rem 8rem', fontSize: '20px', color: 'limegreen'}}>SELECT * FROM 개념 퀴즈;</div>
       </div>
 
-      <div
-        style={{
-          display: 'flex',
-          padding: '5rem',
-          position: 'absolute',
-          top: '120px',
-          right: '40px',
-        }}
-      >
-        <div
-          style={{ marginRight: '3rem', fontSize: '1.8rem', color: 'white' }}
-          onMouseOver={(e) => (e.target.style.color = 'lime')}
-          onMouseOut={(e) => (e.target.style.color = 'white')}
-        >
-          COMPUTER_SCIENCE
-        </div>
-        <div
-          style={{ marginRight: '3rem', fontSize: '1.8rem', color: 'white' }}
-          onMouseOver={(e) => (e.target.style.color = 'lime')}
-          onMouseOut={(e) => (e.target.style.color = 'white')}
-        >
-          JAVA
-        </div>
-        <div
-          style={{ marginRight: '3rem', fontSize: '1.8rem', color: 'white' }}
-          onMouseOver={(e) => (e.target.style.color = 'lime')}
-          onMouseOut={(e) => (e.target.style.color = 'white')}
-        >
-          PYTHON
-        </div>
-        <div
-          style={{ marginRight: '3rem', fontSize: '1.8rem', color: 'white' }}
-          onMouseOver={(e) => (e.target.style.color = 'lime')}
-          onMouseOut={(e) => (e.target.style.color = 'white')}
-        >
-          C
-        </div>
-        <div
-          style={{ fontSize: '1.8rem', color: 'white' }}
-          onMouseOver={(e) => (e.target.style.color = 'lime')}
-          onMouseOut={(e) => (e.target.style.color = 'white')}
-        >
-          C++
-        </div>
+      <div style={{ display: 'flex', padding: '5rem', position: 'absolute', top: '120px', right: '40px'}}>
+        <div style={{ marginRight: '3rem', fontSize: '1.8rem', color: 'white' }} onMouseOver={(e) => (e.target.style.color = 'lime')} onMouseOut={(e) => (e.target.style.color = 'white')}>COMPUTER_SCIENCE</div>
+        <div style={{ marginRight: '3rem', fontSize: '1.8rem', color: 'white' }} onMouseOver={(e) => (e.target.style.color = 'lime')} onMouseOut={(e) => (e.target.style.color = 'white')}>JAVA</div>
+        <div style={{ marginRight: '3rem', fontSize: '1.8rem', color: 'white' }} onMouseOver={(e) => (e.target.style.color = 'lime')} onMouseOut={(e) => (e.target.style.color = 'white')}>PYTHON</div>
+        <div style={{ marginRight: '3rem', fontSize: '1.8rem', color: 'white' }} onMouseOver={(e) => (e.target.style.color = 'lime')} onMouseOut={(e) => (e.target.style.color = 'white')}>C</div>
+        <div style={{ fontSize: '1.8rem', color: 'white' }} onMouseOver={(e) => (e.target.style.color = 'lime')} onMouseOut={(e) => (e.target.style.color = 'white')}>C++</div>
       </div>
 
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-evenly',
-          marginTop: '0rem',
-          marginBottom: '6rem',
-        }}
-      >
-        <Link
-          style={{
-            border: '0.5rem dashed white',
-            padding: '8rem 10rem',
-            fontSize: '2rem',
-          }}
-          to={'/wordquizstart'}
-          className="link-tag"
-        >
-          시작하기
-        </Link>
-        <Link
-          style={{
-            border: '0.5rem dashed white',
-            padding: '8rem 10rem',
-            fontSize: '2rem',
-          }}
-          to={'/wronganswer'}
-          className="link-tag"
-        >
-          내 오답노트
-        </Link>
+      <div style={{ display: 'flex', justifyContent: 'space-evenly', marginTop: '0rem', marginBottom: '6rem'}}>
+        <Link style={{ border: '0.5rem dashed white', padding: '8rem 10rem', fontSize: '2rem'}} to={'/wordquizstart'} className="link-tag">시작하기</Link>
+        <Link style={{ border: '0.5rem dashed white', padding: '8rem 10rem', fontSize: '2rem'}} to={'/wronganswer'} className="link-tag">내 오답노트</Link>
       </div>
     </div>
   );
--- src/components/WrongAnswerRetry.js
+++ src/components/WrongAnswerRetry.js
@@ -1,23 +1,28 @@
 import { useState } from 'react';
 import UserNav from './UserNav';
-import TypingLayout from './layout/typinglayout';
+import LayoutForWrongAnswer from './layout/LayoutForWrongAnswer';
 
 export default function WrongAnswerRetry(){
   const [timer, setTimer] = useState(90);
   return(
-    <div style={{height: '100%'}}>
-      <UserNav/>
-      <TypingLayout timer={timer}>
-        <div style={{display:'flex', flex:6, height:'100%', justifyContent:'center'}}>
-          <div style={{backgroundColor:'red', width:'85%'}}>
-            <div>
-              <p>이 부분 수정해서 만들기</p>
+    <div style={{height:'100%'}}>
+        <div style={{ display: 'flex', position: 'fixed', left: '7%', marginTop:'1.3rem'}}>
+          <div style={{ marginRight: '3rem', fontSize: '1.8rem', color: 'white' }} onMouseOver={(e) => (e.target.style.color = 'lime')} onMouseOut={(e) => (e.target.style.color = 'white')}>COMPUTER_SCIENCE</div>
+          <div style={{ marginRight: '3rem', fontSize: '1.8rem', color: 'white' }} onMouseOver={(e) => (e.target.style.color = 'lime')} onMouseOut={(e) => (e.target.style.color = 'white')}>JAVA</div>
+          <div style={{ marginRight: '3rem', fontSize: '1.8rem', color: 'white' }} onMouseOver={(e) => (e.target.style.color = 'lime')} onMouseOut={(e) => (e.target.style.color = 'white')}>PYTHON</div>
+          <div style={{ marginRight: '3rem', fontSize: '1.8rem', color: 'white' }} onMouseOver={(e) => (e.target.style.color = 'lime')} onMouseOut={(e) => (e.target.style.color = 'white')}>C</div>
+          <div style={{ fontSize: '1.8rem', color: 'white' }} onMouseOver={(e) => (e.target.style.color = 'lime')} onMouseOut={(e) => (e.target.style.color = 'white')}>C++</div>
+      </div>
+        <UserNav/>
+        <LayoutForWrongAnswer timer={timer}>
+          <div style={{display:'flex', flex:6, height:'100%', justifyContent:'center'}}>
+            <div style={{backgroundColor:'red', width:'85%'}}>
+              <div>
+                <p>이 부분 수정</p>
+              </div>
             </div>
           </div>
-        </div>
-      </TypingLayout>
-    </div>
+        </LayoutForWrongAnswer>
+      </div>
   )
 }
-
-// 아직 손 안 댐. 코드 그대로 가져온 상황
