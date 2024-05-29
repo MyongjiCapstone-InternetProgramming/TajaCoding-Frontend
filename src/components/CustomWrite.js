fce9d86c0e1040ea52a543e0f63bb080330dd906
Jinjeongjun <wlswjdwns1@naver.com>
2024-05-30 02:17:33 +0900

    0530 0217 jun

--- src/components/CustomWrite.js
+++ src/components/CustomWrite.js
@@ -1,7 +1,27 @@
 // 담당자 : 은희
+import { useState } from 'react';
 import UserNav from "./UserNav";
 
 export default function CustomWrite(){
+    const [userTitle, setUserTitle] = useState('');
+    const [userDescription, setUserDescription] = useState('');
+    const [userCode, setUserCode] = useState('');
+    const [selectedLanguage, setSelectedLanguage] = useState('java');
+    const id_writer = 1
+
+    const handleRegist = () => {
+        const codeLines = userCode.split('\n');
+
+        const result = {
+            title: userTitle,
+            descript: userDescription,
+            content: codeLines,
+            language: selectedLanguage,
+            writer: id_writer
+        };
+        console.log(result);
+        window.history.back();
+    }
     return(
         <div style={{height:'100%'}}>
             <UserNav/>
@@ -13,6 +33,7 @@ export default function CustomWrite(){
                     <label style={{ color: 'white', fontSize: '20px', display: 'block', marginBottom: '5px' }}>TITLE</label>
                     <input
                         type="text"
+                        onChange={(e) => setUserTitle(e.target.value)}
                         placeholder="제목을 입력하세요."
                         style={{width: '100%',padding: '10px', fontSize: '20px',border: '0.5rem solid white',backgroundColor: 'black',color: 'white',boxSizing: 'border-box'}}/>
                 </div>
@@ -21,17 +42,12 @@ export default function CustomWrite(){
                     <div style={{ display: 'flex', alignItems: 'center' }}> 
                         <input
                             type="text"
+                            onChange={(e) => setUserDescription(e.target.value)}
                             placeholder="간단한 설명을 입력하세요."
                             style={{ flex: 1, width: '100%',padding: '10px',fontSize: '20px',border: '0.5rem solid white',backgroundColor: 'black',color: 'white',boxSizing: 'border-box'}}/>
                         <select
-                            style={{marginLeft: '10px',
-                                    padding: '10px',
-                                    fontSize: '20px',
-                                    border: '0.5rem solid white',
-                                    backgroundColor: 'black',
-                                    color: 'white',
-                                    boxSizing: 'border-box'
-                            }}
+                            onChange={(e) => setSelectedLanguage(e.target.value)}
+                            style={{marginLeft: '10px', padding: '10px', fontSize: '20px', border: '0.5rem solid white', backgroundColor: 'black', color: 'white', boxSizing: 'border-box'}}
                         >
                         <option value="java">JAVA</option>
                         <option value="c++">C++</option>
@@ -47,12 +63,12 @@ export default function CustomWrite(){
                 <div>
                     <textarea
                         placeholder="코드(문제)를 입력하세요."
+                        onChange={(e) => setUserCode(e.target.value)}
                         style={{ width: '100%', height: '350px',padding: '10px',fontSize: '20px',border: '0.5rem dashed white',backgroundColor: 'black',color: 'white',boxSizing: 'border-box'}}>
                     </textarea>
                 </div>
                 <div style={{ display: 'flex', justifyContent: 'space-evenly', marginTop: '2rem', marginBottom: '0rem' }}>
-                <div style={{ fontSize: '2rem' }} className="retry-button-tag" onMouseOver={(e) => (e.target.style.color = 'limegreen')} onMouseOut={(e) => (e.target.style.color = 'white')}>
-                    작성완료</div>
+                <div style={{ fontSize: '2rem' }} className="retry-button-tag" onMouseOver={(e) => (e.target.style.color = 'limegreen')} onMouseOut={(e) => (e.target.style.color = 'white')} onClick={handleRegist}>작성완료</div>
                 </div>
             </div>
         </div>
